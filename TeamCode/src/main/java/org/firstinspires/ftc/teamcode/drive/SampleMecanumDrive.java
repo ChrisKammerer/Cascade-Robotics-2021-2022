c97f3f2038974d4f3dfdfddd2c9dccb2ae059869
ChrisKammerer23 <ckamm001@outlook.com>
2021-12-01 16:56:54 -0500

    PID Fixed and Lateral Multiplier Tuned

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -58,7 +58,7 @@ public class SampleMecanumDrive extends MecanumDrive {
     public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(5, 0, 0);
     public static PIDCoefficients HEADING_PID = new PIDCoefficients(2, 0, 0);
 
-    public static double LATERAL_MULTIPLIER = 1.65;
+    public static double LATERAL_MULTIPLIER = 1.03;// 1.0 for old
 
     public static double VX_WEIGHT = 1;
     public static double VY_WEIGHT = 1;
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RedAutoWarehouse.java
@@ -0,0 +1,97 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.drive.opmode.DuckDetector;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+@Autonomous(name = "Red Warehouse Side", group = "Auto")
+public class RedAutoWarehouse extends LinearOpMode {
+    private ElapsedTime period = new ElapsedTime();
+    private ElapsedTime runtime = new ElapsedTime();
+    public void runToPosition(int count, DcMotor motor, double speed){
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor.setTargetPosition(count);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        period.reset();
+        motor.setPower(1);
+        while((period.seconds()<3) && motor.isBusy()) {
+            //wait
+        }
+        motor.setPower(0);
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    public void delay(double t) { // Imitates the Arduino delay function
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < t)) {
+        }
+    }
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        int cameraMonitorViewId = hardwareMap.appContext
+                .getResources().getIdentifier("cameraMonitorViewId",
+                        "id", hardwareMap.appContext.getPackageName());
+        WebcamName webcamName = hardwareMap.get(WebcamName.class,
+                "webcam");
+        OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+        DuckDetector detector = new DuckDetector(telemetry);
+        camera.setPipeline(detector);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
+            }
+            @Override
+            public void onError(int errorCode)
+            {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+
+        waitForStart();
+        DcMotor liftMotor = hardwareMap.dcMotor.get("liftMotor");
+        Servo bucketServo = hardwareMap.servo.get("bucket");
+        bucketServo.setPosition(0);
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Pose2d startPose = new Pose2d(0,0,0);
+        Trajectory dropoffTraj = null;
+        switch (detector.getLocation()){
+            case LEFT:
+                drive.setPoseEstimate(startPose);
+                Trajectory traj1 = drive.trajectoryBuilder(startPose)
+                        .lineTo(new Vector2d(-17.775, -23.116)).build();
+                drive.followTrajectory(traj1);
+                break;
+            case MIDDLE:
+                break;
+            case RIGHT:
+
+                break;
+        }
+
+    }
+}
